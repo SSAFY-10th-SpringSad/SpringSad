fedc3f08c65a87e8a2471ab19616be2fff1ee16e
KIMSEI1124 <workju1124@gmail.com>
2023-10-10 19:49:25 +0900

    Chore: 리액트 설정

---
+++ frontend/webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'hidden-source-map' : 'eval',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: '[contenthash].bundle.js',
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(__dirname, './public'),
+    port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'esbuild-loader',
+          options: {
+            loader: 'tsx',
+            target: 'es2015',
+          },
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|png|jpe?g|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack', 'file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+    }),
+  ],
+};
