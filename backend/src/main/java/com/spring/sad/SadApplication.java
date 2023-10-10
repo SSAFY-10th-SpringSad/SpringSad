2bfa30970f19d0aead59d6547aa906768197ac80
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-10-10 20:25:14 +0900

    backend init

---
+++ backend/src/main/java/com/spring/sad/SadApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spring/sad/SadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
