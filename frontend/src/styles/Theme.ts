ea715a06c95e86dc3444319b9eb074526b36a686
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-10-13 15:26:58 +0900

    color추가

---
+++ frontend/src/styles/Theme.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+
+const theme = {
+  colors: {
+    mainColorBlue_1: '#1AB0E6',
+    mainColorBlue_2: '#0C5670',
+    mainColorBlue_3: '#083647',
+    mainColorGreen_1: '#19E695',
+    mainColorGreen_2: '#37B381',
+    mainColorGreen_3: '#418067',
+    gray1: '#484A4F',
+    gray2: '#6D7178',
+    grayBackGround1: '#EDF1F7',
+    grayBackGround2: '#CBCED4',
+    black: '#1E1F21',
+  },
+};
+
+export { theme };
