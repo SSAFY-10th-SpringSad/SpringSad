2ed809512d5ae38f217a3a4edeb19f8244562704
손현조 <iijokv@naver.com>
2023-10-17 15:42:17 +0900

    Feat: Profile 도메인 생성

---
+++ backend/src/main/java/com/spring/sad/profile/domain/Profile.java
@@ -0,0 +1,56 @@
+package com.spring.sad.profile.domain;
+
+import com.spring.sad.member.domain.Member;
+import com.spring.sad.member.domain.MemberBand;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class Profile {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String imgSrc;
+
+    private boolean isDefault;
+
+    @OneToMany(mappedBy = "profile")
+    private List<MemberBand> memberBandList = new ArrayList<>();
+
+    @Builder
+    public Profile(
+            int id,
+            String name,
+            String imgSrc,
+            boolean isDefault,
+            Member member) {
+        this.id = id;
+        this.name = name;
+        this.imgSrc = imgSrc;
+        this.isDefault = isDefault;
+        addRelatedMember(member);
+    }
+
+    private void addRelatedMember(Member member) {
+        this.member = member;
+        member.getProfileList().add(this);
+    }
+}
---
+++ backend/src/main/java/com/spring/sad/profile/domain/ProfileRepository.java
@@ -0,0 +1,6 @@
+package com.spring.sad.profile.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+}
