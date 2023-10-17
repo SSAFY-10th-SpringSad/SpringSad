52f1c6480ed4bb8eaff1220a1d66d6a510bb8099
손현조 <iijokv@naver.com>
2023-10-17 20:58:33 +0900

    Feat: Enum 싱글톤 설정

--- backend/src/main/java/com/spring/sad/band/domain/enums/VisibilityEnum.java
+++ backend/src/main/java/com/spring/sad/band/domain/enums/VisibilityEnum.java
@@ -1,7 +1,19 @@
 package com.spring.sad.band.domain.enums;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public enum VisibilityEnum {
-    PUBLIC,     // PUBLIC: 공개
-    PROTECTED,  // PROTECTED: 밴드명 공개
-    PRIVATE     // PRIVATE: 비공개
+    PUBLIC("공개"),
+    NAMEONLY("밴드명 공개"),
+    PRIVATE("비공개");
+
+    private String value;
+
+    VisibilityEnum(String value) {
+        this.value = value;
+    }
 }
--- backend/src/main/java/com/spring/sad/member/domain/enums/GenderEnum.java
+++ backend/src/main/java/com/spring/sad/member/domain/enums/GenderEnum.java
@@ -1,6 +1,18 @@
 package com.spring.sad.member.domain.enums;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public enum GenderEnum {
-    M,
-    F
+    MALE("남"),
+    FEMALE("여");
+
+    private String value;
+
+    GenderEnum(String value) {
+        this.value = value;
+    }
 }
