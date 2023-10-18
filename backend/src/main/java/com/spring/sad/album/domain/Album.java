3b6a9c344199fcf81b6a687a6e9c8905bd6c34b7
muuuumbi <dnpfkdlvm123@gmail.com>
2023-10-18 18:53:45 +0900

    Album 코드 추가

--- backend/src/main/java/com/spring/sad/album/domain/Album.java
+++ backend/src/main/java/com/spring/sad/album/domain/Album.java
@@ -1,19 +1,36 @@
 package com.spring.sad.album.domain;
 
 import com.spring.sad.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import com.spring.sad.image.domain.AlbumImage;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Album extends BaseTimeEntity {
+    @Id
     @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int albumId;
 
     @Column(nullable = false)
     private int bandId;
+
+    @OneToMany(mappedBy = "album", fetch = FetchType.LAZY)
+    private List<AlbumImage> albumImages = new ArrayList<>();
+
+    @Builder
+    public Album(int albumId, int bandId){
+        this.albumId = albumId;
+        this.bandId = bandId;
+    }
 }
--- backend/src/main/java/com/spring/sad/album/repository/AlbumRepository.java
+++ backend/src/main/java/com/spring/sad/album/repository/AlbumRepository.java
@@ -4,5 +4,4 @@ import com.spring.sad.album.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
-    //복합키는 어떻게?
 }
