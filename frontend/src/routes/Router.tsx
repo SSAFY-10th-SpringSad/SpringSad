c64d9d942fe36b1a59740ae764ee643f7cf6b734
wooryjoon <wooryjoon@gmail.com>
2023-10-20 20:06:44 +0900

    이메일 회원가입 UI 구현

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -3,16 +3,19 @@ import BandDetailPage from '@/pages/BandDetailPage/BandDetailPage';
 import CreateBandPage from '@/pages/CreateBandPage/CreateBandPage';
 import HomePage from '@/pages/HomePage/HomePage';
 import LoginPage from '@/pages/LoginPage/LoginPage';
-import SignUpPage from '@/pages/SignUpPage/SignUpPage';
+import { BROWSER_PATH } from '@/constants/path';
+import PhoneSignUpPage from '@/pages/SignUpPage/PhoneSignUpPage/PhoneSignUpPage';
+import EmailSignUpPage from '@/pages/SignUpPage/EmailSignUpPage/EmailSignUpPage';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      <Route path={BROWSER_PATH.HOME} element={<HomePage />} />
+      <Route path={BROWSER_PATH.PHONE_LOGIN} element={<LoginPage />} />
+      <Route path={BROWSER_PATH.PHONE_SIGN_UP} element={<PhoneSignUpPage />} />
+      <Route path={BROWSER_PATH.EMAIL_SIGN_UP} element={<EmailSignUpPage />} />
       <Route path="/band:bandId" element={<BandDetailPage />} />
-      <Route path="/createBand" element={<CreateBandPage />} />
+      <Route path={BROWSER_PATH.CREATE_BAND} element={<CreateBandPage />} />
     </Routes>
   );
 }
