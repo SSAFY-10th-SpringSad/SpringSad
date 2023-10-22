ed480a773fbd71be173c72ca0ea8c696046d9301
lkt9899 <lkt9899@naver.com>
2023-10-22 23:52:04 +0900

    feat : 회원 가입, 로그인 관련 쿼리 추가

--- backend/src/main/java/com/spring/sad/member/repository/MemberRepository.java
+++ backend/src/main/java/com/spring/sad/member/repository/MemberRepository.java
@@ -1,7 +1,18 @@
 package com.spring.sad.member.repository;
 
 import com.spring.sad.member.domain.Member;
+import com.spring.sad.member.domain.embedded.Email;
+import com.spring.sad.member.domain.embedded.PhoneNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 회원 가입 시 중복 검사
+    boolean existsByEmail(Email email);
+    boolean existsByPhoneNumber(PhoneNumber phoneNumber);
+
+    // 로그인 시 회원 정보 가져오기
+    Optional<Member> findByEmail(Email email);
+    Optional<Member> findByPhoneNumber(PhoneNumber phoneNumber);
 }
