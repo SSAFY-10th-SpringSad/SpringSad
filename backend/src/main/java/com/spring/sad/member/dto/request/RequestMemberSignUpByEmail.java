7738482db890a82914bcb14b1f2d379e116ea2f3
lkt9899 <lkt9899@naver.com>
2023-10-23 01:37:15 +0900

    style : 클래스 명 변경

--- backend/src/main/java/com/spring/sad/member/dto/request/RequestMemberSignUpByEmail.java
+++ backend/src/main/java/com/spring/sad/member/dto/request/RequestMemberSignUpByEmail.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
-public class RequestMemberSignUpByEmailDto extends RequestMemberDto {
+public class RequestMemberSignUpByEmail extends RequestMemberDto {
     private String email;
 
     @Override
