b89d164886a0ab784a42482a63cd8e2df639c3a2
lkt9899 <lkt9899@naver.com>
2023-10-23 01:37:55 +0900

    refactor : 사용되지 않는 import 제거

--- backend/src/main/java/com/spring/sad/member/controller/MemberController.java
+++ backend/src/main/java/com/spring/sad/member/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.spring.sad.member.controller;
 
-import com.spring.sad.member.dto.request.RequestMemberDto;
 import com.spring.sad.member.dto.request.RequestMemberSignUpByEmail;
 import com.spring.sad.member.dto.request.RequestMemberSignUpByPhoneNumber;
 import com.spring.sad.member.dto.response.ResponseMemberDto;
@@ -8,7 +7,10 @@ import com.spring.sad.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/member")
 @RequiredArgsConstructor
