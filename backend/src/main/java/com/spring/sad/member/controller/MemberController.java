b7e97ca3bf011366f36f01a29ab955240d779e4d
손현조 <iijokv@naver.com>
2023-10-22 22:27:21 +0900

    Feat: MemberController 구현  - 회원 가입 로직 추가

---
+++ backend/src/main/java/com/spring/sad/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.spring.sad.member.controller;
+
+import com.spring.sad.member.dto.request.MemberSignupByCellPhoneRequest;
+import com.spring.sad.member.dto.request.MemberSignupByEmailRequest;
+import com.spring.sad.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/member")
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/signup/email")
+    public ResponseEntity<Void> signup(@RequestBody MemberSignupByEmailRequest request) {
+        memberService.signupByEmail(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signup/cellphone")
+    public ResponseEntity<Void> signup(@RequestBody MemberSignupByCellPhoneRequest request) {
+        memberService.signupByCellPhone(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
