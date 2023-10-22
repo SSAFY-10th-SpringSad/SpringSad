951c01fb1643eefc529365c3eb6eae1f72f2d596
lkt9899 <lkt9899@naver.com>
2023-10-22 23:53:10 +0900

    fix : Auditing Annotation 추가

--- backend/src/main/java/com/spring/sad/SadApplication.java
+++ backend/src/main/java/com/spring/sad/SadApplication.java
@@ -2,7 +2,10 @@ package com.spring.sad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class SadApplication {
 
