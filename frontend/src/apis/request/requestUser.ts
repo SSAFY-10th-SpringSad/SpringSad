b4e53770e67014bf8ed14858c99276c5f5dddf12
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-10-23 10:49:19 +0900

    refactor:signup 타입 두가지합침

--- frontend/src/apis/request/requestUser.ts
+++ frontend/src/apis/request/requestUser.ts
@@ -1,7 +1,9 @@
 import { axios } from '../axios';
 import { API_PATH } from '@/constants/path';
 
-const requestSignUp = (userData: RequestSignupUserType) => {
+const requestSignUp = (
+  userData: RequestEmailSignupUserType | RequestPhoneSignupUserType,
+) => {
   return axios.post(API_PATH.USER.SIGN_UP, userData);
 };
 
