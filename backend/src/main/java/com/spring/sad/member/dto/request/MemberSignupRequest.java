a9e1cb2b82b7e720dfd37618aa3a06c455b18947
손현조 <iijokv@naver.com>
2023-10-24 17:29:05 +0900

    Fix: @Getter 미적용으로 인한 필드 접근 불가 해결

--- backend/src/main/java/com/spring/sad/member/dto/request/MemberSignupRequest.java
+++ backend/src/main/java/com/spring/sad/member/dto/request/MemberSignupRequest.java
@@ -1,7 +1,9 @@
 package com.spring.sad.member.dto.request;
 
 import com.spring.sad.member.domain.Member;
+import lombok.Getter;
 
+@Getter
 public abstract class MemberSignupRequest {
     protected String password;
     protected String name;
