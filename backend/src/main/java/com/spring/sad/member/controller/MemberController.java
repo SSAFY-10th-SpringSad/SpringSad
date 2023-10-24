0a8aac5c5ab08933bec1c1a4ab98b721234ed31d
muuuumbi <dnpfkdlvm123@gmail.com>
2023-10-24 17:31:35 +0900

    feat: memberController

--- backend/src/main/java/com/spring/sad/member/controller/MemberController.java
+++ backend/src/main/java/com/spring/sad/member/controller/MemberController.java
@@ -1,2 +1,32 @@
-package com.spring.sad.member.controller;public class MemberController {
+package com.spring.sad.member.controller;
+
+import com.spring.sad.member.dto.requestDto.RequestMemberSignUpByCellPhoneDto;
+import com.spring.sad.member.dto.requestDto.RequestMemberSignUpByEmailDto;
+import com.spring.sad.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor //@Autowired를 사용하지 않고 의존성 주입
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+    @PostMapping("/signup/email")
+    public ResponseEntity<Void> signUpByEmail(@RequestBody RequestMemberSignUpByEmailDto request){
+        memberService.signUpByEmail(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/signup/cellphone")
+    public ResponseEntity<Void> signUpByCellPhone(@RequestBody RequestMemberSignUpByCellPhoneDto request){
+        memberService.signUpByCellPhone(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 }
