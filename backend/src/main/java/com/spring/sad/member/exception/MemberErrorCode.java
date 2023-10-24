5ce2373f4c8e0e5ffce576a8eb896716e06ccfbe
muuuumbi <dnpfkdlvm123@gmail.com>
2023-10-24 17:29:33 +0900

    feat: exception

--- backend/src/main/java/com/spring/sad/member/exception/MemberErrorCode.java
+++ backend/src/main/java/com/spring/sad/member/exception/MemberErrorCode.java
@@ -1,2 +1,25 @@
-package com.spring.sad.member.exception;public enum MemberErrorCode {
+package com.spring.sad.member.exception;
+
+import com.spring.sad.global.exception.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum MemberErrorCode implements ErrorCode {
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST.value(), "DUPLICATE_EMAIL", "이미 가입된 이메일입니다."),
+    DUPLICATE_PHONE_NUMBER(HttpStatus.BAD_REQUEST.value(), "DUPLICATE_PHONE_NUMBER", "이미 가입된 핸드폰 번호입니다."),
+    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST.value(), "INVALID_EMAIL_FORMAT", "잘못된 이메일 형식입니다."),
+    INVALID_CELL_PHONE_FORMAT(HttpStatus.BAD_REQUEST.value(), "INVALID_PHONE_NUMBER_FORMAT", "잘못된 핸드폰 번호 형식입니다."),
+    INVALID_DATE_FORMAT(HttpStatus.BAD_REQUEST.value(), "INVALID_DATE_FORMAT", "잘못된 날짜 형식입니다."),
+    LOGIN_FAILED(HttpStatus.BAD_REQUEST.value(), "LOGIN_FAILED", "로그인 정보를 확인해주세요.");
+
+    private final int statusCode;
+    private final String errorCode;
+    private String message;
+
+    MemberErrorCode(int statusCode, String errorCode, String message){
+        this.statusCode = statusCode;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
 }
