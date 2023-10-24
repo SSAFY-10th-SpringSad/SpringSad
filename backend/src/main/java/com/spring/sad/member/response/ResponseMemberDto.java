8cad5addd030e5ffb05f284342530562a839c4bc
muuuumbi <dnpfkdlvm123@gmail.com>
2023-10-24 17:30:02 +0900

    feat: responseDto

--- backend/src/main/java/com/spring/sad/member/response/ResponseMemberDto.java
+++ backend/src/main/java/com/spring/sad/member/response/ResponseMemberDto.java
@@ -1,2 +1,24 @@
-package com.spring.sad.member.response;public class ResponseMemberDto {
+package com.spring.sad.member.response;
+
+import com.spring.sad.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ResponseMemberDto {
+    private int id;
+    private String name;
+    private String password;
+
+    private ResponseMemberDto(Member member){
+        this.id = member.getId();
+        this.name = member.getName();
+        this.password = member.getPassword();
+    }
+
+    public static ResponseMemberDto of(Member member){
+        return new ResponseMemberDto(member);
+    }
 }
