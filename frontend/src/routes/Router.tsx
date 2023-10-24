1bf29d6c4e569c45988a05f4570cf3e3dc320565
wooryjoon <wooryjoon@gmail.com>
2023-10-24 21:11:26 +0900

    refactor : react-router-dom refactoring

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -5,15 +5,39 @@ import HomePage from '@/pages/HomePage/HomePage';
 import LoginPage from '@/pages/LoginPage/LoginPage';
 import { BROWSER_PATH } from '@/constants/path';
 import SignUpPage from '@/pages/SignUpPage/SignUpPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-export default function Router() {
-  return (
-    <Routes>
-      <Route path={BROWSER_PATH.HOME} element={<HomePage />} />
-      <Route path={BROWSER_PATH.LOGIN} element={<LoginPage />} />
-      <Route path={BROWSER_PATH.SIGN_UP} element={<SignUpPage />} />
-      <Route path="/band:bandId" element={<BandDetailPage />} />
-      <Route path={BROWSER_PATH.CREATE_BAND} element={<CreateBandPage />} />
-    </Routes>
-  );
-}
+const Router = createBrowserRouter([
+  {
+    path: BROWSER_PATH.HOME,
+    element: <HomePage />,
+  },
+  {
+    path: BROWSER_PATH.LOGIN,
+    element: <LoginPage />,
+  },
+  {
+    path: BROWSER_PATH.SIGN_UP,
+    element: <SignUpPage />,
+  },
+  {
+    path: BROWSER_PATH.CREATE_BAND,
+    element: <CreateBandPage />,
+  },
+  {
+    path: '/band:bandId',
+    element: <BandDetailPage />,
+  },
+]);
+export default Router;
+// export default function Router() {
+//   return (
+//     <Routes>
+//       <Route path={BROWSER_PATH.HOME} element={<HomePage />} />
+//       <Route path={BROWSER_PATH.LOGIN} element={<LoginPage />} />
+//       <Route path={BROWSER_PATH.SIGN_UP} element={<SignUpPage />} />
+//       <Route path="/band:bandId" element={<BandDetailPage />} />
+//       <Route path={BROWSER_PATH.CREATE_BAND} element={<CreateBandPage />} />
+//     </Routes>
+//   );
+// }
