c01951fc8eb8401fbf421b19faabdbf6adb48cc6
muuuumbi <dnpfkdlvm123@gmail.com>
2023-10-24 17:30:57 +0900

    feat: globalException

--- backend/src/main/java/com/spring/sad/global/exception/ControllerAdvice.java
+++ backend/src/main/java/com/spring/sad/global/exception/ControllerAdvice.java
@@ -1,2 +1,14 @@
-package com.spring.sad.global.exception;public class ControllerAdvice {
+package com.spring.sad.global.exception;
+
+import com.spring.sad.global.exception.dto.ExceptionResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice //@RestController로 선언한 지점에서 발생한 에러를 catch해 처리해줌
+public class ControllerAdvice {
+    @ExceptionHandler(GlobalException.class) //특정 에러 발생 시 오류 반환하도록 처리
+    public ResponseEntity<ExceptionResponse> handleException(GlobalException e){
+        return ResponseEntity.status(e.getStatusCode()).body(ExceptionResponse.from(e));
+    }
 }
--- backend/src/main/java/com/spring/sad/global/exception/dto/ExceptionResponse.java
+++ backend/src/main/java/com/spring/sad/global/exception/dto/ExceptionResponse.java
@@ -1,2 +1,19 @@
-package com.spring.sad.global.exception.dto;public class ExceptionResponse {
+package com.spring.sad.global.exception.dto;
+
+import com.spring.sad.global.exception.GlobalException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Data
+public class ExceptionResponse {
+    private String message;
+    private String errorCode;
+
+    public static ExceptionResponse from(GlobalException e){
+        return new ExceptionResponse(e.getMessage(), e.getErrorCode());
+    }
 }
